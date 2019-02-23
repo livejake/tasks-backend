13e091f66d03b9256cc98378b9af42411ff3f96c
Jake Bialer <jakebialer@Jakes-MacBook-Pro-2.local>
2019-02-23 14:20:25 -0500

    Add interval type, update other objects to handle, and add task resolver

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const mongoose = require('mongoose');
 const db = require("./app")
 const app = express();
 
+const IntervalType = new graphql.GraphQLObjectType({
+  name: 'Interval',
+  fields: {
+    _id: { type: graphql.GraphQLID },
+    name: { type: graphql.GraphQLString },
+  }
+});
+
 const UserType = new graphql.GraphQLObjectType({
   name: 'User',
   fields: {
@@ -14,11 +22,20 @@ const UserType = new graphql.GraphQLObjectType({
   }
 });
 
-const TaskType = new  graphql.GraphQLObjectType({
+const TaskType = new graphql.GraphQLObjectType({
   name: 'Task',
   fields: {
     _id: { type: graphql.GraphQLID },
-    interval: { type: graphql.GraphQLID },
+    interval: {
+      type: IntervalType,
+      async resolve(parentValue) {
+        return db.Interval.findById(parentValue.interval, function (err, interval) {
+          if (err) return console.error(err);
+          return interval
+        })
+      }
+
+    },
     name: { type: graphql.GraphQLString },
     points: { type: graphql.GraphQLInt },
   }
@@ -28,25 +45,25 @@ const ActivityLogType = new graphql.GraphQLObjectType({
   name: "ActivityLog",
   fields: {
     _id: { type: graphql.GraphQLID },
-    task: { 
+    task: {
       type: TaskType,
-      async resolve(parentValue){
-        return db.Task.findById(parentValue.task, function(err, task){
+      async resolve(parentValue) {
+        return db.Task.findById(parentValue.task, function (err, task) {
           if (err) return console.error(err);
           return task
-        }) 
+        })
       }
-     },
-    user: { 
+    },
+    user: {
       type: UserType,
       async resolve(parentValue) {
         return db.User.findById(parentValue.user, function (err, user) {
           if (err) return console.error(err);
           return user
         })
-      }  
+      }
     },
-    action: { type: graphql.GraphQLString},
+    action: { type: graphql.GraphQLString },
     timestamp: { type: graphql.GraphQLInt }
   }
 })
@@ -57,27 +74,57 @@ const Query = new graphql.GraphQLObjectType({
     users: {
       type: new graphql.GraphQLList(UserType),
       args: {},
-      async resolve (parentValue, args) {
+      async resolve(parentValue, args) {
         return db.User.find({}, function (err, users) {
           if (err) return console.error(err);
           return users
         })
       }
     },
+    intervals: {
+      type: new graphql.GraphQLList(IntervalType),
+      args: {},
+      async resolve(parentValue, args) {
+        return db.Interval.find({}, function (err, intervals) {
+          if (err) return console.error(err);
+          return intervals
+        })
+      }
+    },
+    interval: {
+      type: IntervalType,
+      args: { id: { type: new graphql.GraphQLNonNull(graphql.GraphQLID) } },
+      async resolve(parentValue, args) {
+        return db.Interval.findById(args.id, function (err, interval) {
+          if (err) return console.error(err);
+          return interval
+        })
+      }
+    },
+    tasks: {
+      type: new graphql.GraphQLList(TaskType),
+      args: {},
+      async resolve(parentValue, args) {
+        return db.Task.find({}, function (err, tasks) {
+          if (err) return console.error(err);
+          return tasks
+        })
+      }
+    },
     user: {
       type: UserType,
-      args: {id: { type: new graphql.GraphQLNonNull(graphql.GraphQLID) } },
-      async resolve (parentValue, args) {
+      args: { id: { type: new graphql.GraphQLNonNull(graphql.GraphQLID) } },
+      async resolve(parentValue, args) {
         return db.User.findById(args.id, function (err, user) {
           if (err) return console.error(err);
           return user
         })
       }
     },
-    activityLog : {
+    activityLog: {
       type: new graphql.GraphQLList(ActivityLogType),
       args: {},
-      async resolve (parentValue, args){
+      async resolve(parentValue, args) {
         return db.ActivityLog.find({}, function (err, activityLogs) {
           if (err) return console.error(err);
           return activityLogs
@@ -88,7 +135,7 @@ const Query = new graphql.GraphQLObjectType({
   }
 })
 
-let schema = new graphql.GraphQLSchema({query: Query})
+let schema = new graphql.GraphQLSchema({ query: Query })
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
@@ -148,7 +195,7 @@ var root = {
 //     _id: ID 
 //     name: String
 //   }
-  
+
 //   type activityLog{
 //     _id: ID 
 //     action: String
